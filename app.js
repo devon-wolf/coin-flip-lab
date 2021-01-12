e291bffe91e8db76e04f55c0e61888ad00b9ed24
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-12 14:26:57 -0800

    Decrement remaining guesses when guessButton is clicked, update message to user with correct count

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // fluctuating variables
 let userInput = document.getElementById('user-guess');
-let guessesRemaining = document.getElementById('remaining-guesses');
+let guessCounter = document.getElementById('remaining-guesses');
 
 // display areas
 const evalBox = document.getElementById('guess-eval');
@@ -11,16 +11,21 @@ const endGameBox = document.getElementById('game-over');
 const guessButton = document.getElementById('submit-guess');
 const resetButton = document.getElementById('reset-button');
 
-console.log(userInput, guessesRemaining, evalBox, counterBox, endGameBox, guessButton, resetButton);
+// console.log(userInput, guessCounter, evalBox, counterBox, endGameBox, guessButton, resetButton);
 
 // initialize state
-guessesRemaining = 4;
+let randomNumber = Math.ceil(Math.random() * 20);
+let guessesRemaining = 4;
+
+// console.log(randomNumber, guessesRemaining);
 
 // set event listeners to update state and DOM
 guessButton.addEventListener('click', () => {
-    console.log('Someone clicked the guess button.');
+	// console.log('Someone clicked the guess button.');
+    guessesRemaining--;
+    guessCounter.textContent = guessesRemaining;
 });
 
 resetButton.addEventListener('click', () => {
-    console.log('Someone clicked the reset button.');
+    // console.log('Someone clicked the reset button.');
 });
