41f354a2807f5e48eab8e688f593ba4a1192d6cc
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-12 14:10:39 -0800

    Confirm all elements are accessible by the app, event listeners are working

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ const endGameBox = document.getElementById('game-over');
 const guessButton = document.getElementById('submit-guess');
 const resetButton = document.getElementById('reset-button');
 
-
-
-
+console.log(userInput, guessesRemaining, evalBox, counterBox, endGameBox, guessButton, resetButton);
 
 // initialize state
+guessesRemaining = 4;
 
 // set event listeners to update state and DOM
+guessButton.addEventListener('click', () => {
+    console.log('Someone clicked the guess button.');
+});
+
+resetButton.addEventListener('click', () => {
+    console.log('Someone clicked the reset button.');
+});
