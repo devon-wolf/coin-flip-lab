789a220d0eba38d5694d51883e6db0bd104820f0
Devon Wolfkiel <devonwolf@tutanota.com>
2021-01-12 14:05:12 -0800

    Add DOM elements to app

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-// import functions and grab DOM elements
+// fluctuating variables
+let userInput = document.getElementById('user-guess');
+let guessesRemaining = document.getElementById('remaining-guesses');
+
+// display areas
+const evalBox = document.getElementById('guess-eval');
+const counterBox = document.getElementById('guess-counter');
+const endGameBox = document.getElementById('game-over');
+
+// buttons
+const guessButton = document.getElementById('submit-guess');
+const resetButton = document.getElementById('reset-button');
+
+
+
+
 
 // initialize state
 
